fd6a57582793b20d119aa4a1ae0ddeca17041128
AlexanderJQA <aoronosanchez@gmail.com>
2025-05-21 23:59:22 +0100

    Correcion de un error

--- cypress/e2e/1-getting-started/test.cy.js
+++ cypress/e2e/1-getting-started/test.cy.js
@@ -1,5 +1,5 @@
  describe('Pruebas de cypress',() => {
-    it("visitar la pagina y marcar una tarea como terminda",()=>{
+    it("visitar la pagina y marcar una tarea como terminada",()=>{
     cy.visit("https://example.cypress.io/todo")
     cy.get('.todo-list li').first().get(".toggle").first().click()
  })
